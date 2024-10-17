84a2703ad60899279f7a28511fba6791be3bbb65
Amose Romeo Buzzi <68696888+Amose3535@users.noreply.github.com>
2024-10-18 00:49:31 +0200

    applied modifications

--- Scripts/MainView.gd
+++ Scripts/MainView.gd
@@ -1,6 +1,4 @@
 extends Control
-
-
 @onready var status_label: RichTextLabel = $Status
 @onready var url_field: LineEdit = $URL_Field
 @onready var download_field: LineEdit = $Download_Field
@@ -8,11 +6,7 @@ extends Control
 @onready var video_download_button: Button = $Video_Download
 @onready var audio_download_button: Button = $Audio_Download
 
-
-
-var is_everything_ready : bool = false
-var download
-
+var download = null
 func  _ready() -> void:
 	video_download_button.disabled = true
 	audio_download_button.disabled = true
@@ -21,15 +15,29 @@ func  _ready() -> void:
 		await  YtDlp.setup_completed
 		status_label.text = "Status: [color=green]READY[/color]"
 		video_download_button.disabled = false
-		audio_download_button.disabled = false
+		audio_download_button.disabled = false	
 
 func _on_video_download_pressed() -> void:
+	# Download video function
+
+	# Disable buttons to prevent multiple downloads
 	video_download_button.disabled = true
 	audio_download_button.disabled = true
+	# Start download
 	download = YtDlp.download(url_field.text).set_destination(download_field.text).set_file_name(download_name.text).start()
-	assert(download.get_status() == YtDlp.Download.Status.DOWNLOADING)
-	status_label.text = "Status: [color=orange]Downlaoding[/color]"
-	await download.download_completed
-	status_label.text = "Status: [color=green]Done[/color]"
+	# Assert that the download is in progress, if not then the download failed (function will stop)
+	if download.get_status() != YtDlp.Download.Status.DOWNLOADING:
+		status_label.text = "Status: [color=red]Download failed[/color]"
+		return
+	else:
+		# set the status label to downloading
+		status_label.text = "Status: [color=orange]Downlaoding[/color]"
+		# Wait for the download to complete
+		await download.download_completed
+		# Set the status label to done after the download is completed
+		status_label.text = "Status: [color=green]Done[/color]"
+		# Enable the buttons after the download is completed
+		video_download_button.disabled = false
+		audio_download_button.disabled = false
 	
 	
